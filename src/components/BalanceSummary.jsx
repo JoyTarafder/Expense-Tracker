55ce612450fbe6605a8d60f8bef0f4a74e611211
Joy Tarafder <67510333+JoyTarafder@users.noreply.github.com>
2024-10-17 14:41:27 +0600

    balance edit

--- src/components/BalanceSummary.jsx
+++ src/components/BalanceSummary.jsx
@@ -12,15 +12,18 @@ export default function BalanceSummary({ expenseForm, incomeFrom }) {
       <dl className="grid grid-cols-1 text-center lg:grid-cols-3 divide-x-2 border rounded-md overflow-hidden">
         <div className="bg-[#F9FAFB] flex lg:max-w-xs flex-col px-4 py-4">
           <dt className="text-base leading-7 text-gray-600">Balance</dt>
-          {Balance > 0 ? (
-            <dd className="order-first text-xl font-semibold tracking-tight text-gray-700 sm:text-3xl">
-              BDT {Balance}
-            </dd>
-          ) : (
-            <dd className="order-first text-xl font-semibold tracking-tight text-red-700 sm:text-3xl">
-              BDT {Balance}
-            </dd>
-          )}
+          <dd className="order-first text-xl font-semibold tracking-tight text-gray-700 sm:text-3xl">
+            BDT{" "}
+            {Balance > -1 ? (
+              <span className="order-first text-xl font-semibold tracking-tight text-gray-700 sm:text-3xl">
+                {Balance}
+              </span>
+            ) : (
+              <span className="order-first text-xl font-semibold tracking-tight text-red-500 sm:text-3xl">
+                {Balance}
+              </span>
+            )}
+          </dd>
         </div>
         <div className="bg-[#F9FAFB] flex lg:max-w-xs flex-col px-4 py-4">
           <dt className="text-base leading-7 text-gray-600">Total Income</dt>
